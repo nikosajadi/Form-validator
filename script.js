de5c59ec505fab7c2cd9e56ce782bf2c4fe78d7b
nikosajadi <na.sajadii.29@gmail.com>
2023-02-05 13:47:35 +0100

    Style main message for error messages

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const form = document.getElementById('form');
 const password1EL = document.getElementById('password1');
 const password2EL = document.getElementById('password2');
-const messageContainer = document.getElementById('message-container');
+const messageContainer = document.getElementById('.message-container');
+const message = document.getElementById('message');
 
 
 // I am going to be to validate the form itself
@@ -11,6 +12,10 @@ function validateForm() {
     //Using Contraint API to validate
     isvalid = form.checkVisibility();
     console.log(isvalid);
+    //Style main message for error messages
+    message.textContent = 'please fill out all fields.';
+    message.style.color = 'red';
+    messageContainer.style.borderColor = 'red';
 }
 
 
