7b3fe209a705c12ce33f99fd9f88172b99ed8765
nikosajadi <na.sajadii.29@gmail.com>
2023-02-07 23:19:29 +0100

    passwordMatch

--- script.js
+++ script.js
@@ -7,14 +7,30 @@ const message = document.getElementById('message');
 
 // I am going to be to validate the form itself
 let isValid = false;
+let passwordMatch = false;
 
 function validateForm() {
     // Use HTML constraint API to check form validity
     isValid = form.checkValidity();
       // Style main message for an error
-      message.textContent = 'Please fill out all fields.';
-      message.style.color = 'red';
-      messageContainer.style.borderColor = 'red';
+      if (!isValid) {
+        message.textContent = 'Please fill out all fields.';
+        message.style.color = 'red';
+        messageContainer.style.borderColor = 'red';
+      }
+ // check to see if password match
+ if (password1El.value === password2El.value) {
+    passwordMatch = true;
+    password1El.style.borderColor = 'green'
+    password1El.style.borderColor = 'green'
+} else {
+    passwordMatch = false;
+    message.textContent='Make sure passwords match';
+    message.style.color = 'red';
+    messageContainer.style.borderColor = 'red';
+    password1El.style.borderColor = 'red';
+    password1El.style.borderColor = 'red';
+}
 }
 function processFormData(e) {
     e.preventDefault();
